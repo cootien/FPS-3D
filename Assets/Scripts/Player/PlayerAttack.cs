20791e33b51c6d2be5e662b882c7484a26c1c2b0
Tien <127644559+cootien@users.noreply.github.com>
2024-06-12 12:38:01 +0700

    Enemy blood effect

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -8,22 +8,72 @@ public class PlayerAttack : Singleton<PlayerAttack>
 {
     [SerializeField] private Animator animator;
     [SerializeField] private UnityEvent onAttack;
+    [SerializeField] private Weapon weapon;
+
+    public float attackDistance = 5f;
+    private float attackDelay = 8f;
 
     public int damage;
 
     public AudioSource WhoosingSound;
-    public AudioSource StabbingSound;
+
+
+    private bool isAttacking;
+    public bool IsAttacking => isAttacking;
+
 
     public void StartAttack()
     {
         animator.SetBool("Attack", true);
         WhoosingSound.Play();
+        isAttacking = true;
+
+        PerformWeaponCheck();
+        //StartCoroutine(DelayedAttack());
+
     }
 
     public void StopAttack()
     {
         animator.SetBool("Attack", false);
+        isAttacking = false;
+      
     }
+    private void PerformWeaponCheck()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, attackDistance);
+        foreach (Collider collider in colliders)
+        {
+            weapon.ShowHitEffect(collider);
+            if (collider.CompareTag("Enemy"))
+            {
+                weapon.DeliverDamage(collider);
+                Debug.Log("=======Blood");
+            }
+        }
+    }
+    //private IEnumerator DelayedAttack()
+    //{
+    //    yield return new WaitForSeconds(attackDelay);
+    //    isAttacking = true;
+
+    //    // Simulate the duration of the attack (adjust this duration as needed)
+    //    yield return new WaitForSeconds(0.1f);
+
+    //    isAttacking = false;
+    //}
+
+    //void AttackRaycast()
+    //{
+    //    if (Physics.Raycast(weapon.transform.position, weapon.transform.forward, out RaycastHit hit, attackDistance))
+    //    {
+    //        weapon.ShowHitEffect(hit);
+
+    //        if (hit.collider.CompareTag("Enemy"))
+    //        { weapon.DeliverDamage(hit.collider); }
+    //    }
+    //}
+
 
     //public void DeliverDamage(Collider enemy)
     //{
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -9,38 +9,21 @@ public class Weapon : MonoBehaviour
     public AudioSource StabbingSound;
     private GameObject hitMarkerPrefab;
 
-    private void Start()
-    {
-        Debug.Log("=== weapon start");
 
-    }
-    private void Update()
-    {
-        Debug.Log("=== weapon update");
-    }
-
-    //private void OnCollisionEnter(Collision collision)
+    //private void OnTriggerEnter(Collider collider)
     //{
-    //    Debug.Log("==== weapon");
-    //    ShowHitEffect(collision.collider);
-
-    //    if (collision.gameObject.CompareTag("Enemy"))
+    //    if (PlayerAttack.Instance.IsAttacking)
     //    {
-    //        Debug.Log("=======Blood");
-    //        DeliverDamage(collision.collider);
+    //        Debug.Log("===Player attacking & enter collider");
+    //        ShowHitEffect(collider);
+
+    //        if (collider.CompareTag("Enemy"))
+    //        {s
+    //            Debug.Log("=======Blood");
+    //            DeliverDamage(collider);
+    //        }
     //    }
     //}
-    private void OnTriggerEnter(Collider collider)
-    {
-        Debug.Log("==== weapon");
-        ShowHitEffect(collider);
-
-        if (collider.CompareTag("Enemy"))
-        {
-            Debug.Log("=======Blood");
-            DeliverDamage(collider);
-        }
-    }
 
     public void DeliverDamage(Collider enemy)
     {
@@ -52,16 +35,31 @@ public class Weapon : MonoBehaviour
             health.TakeDamage(damage);
         }
     }
-
-    private void ShowHitEffect(Collider collider)
+    public void ShowHitEffect(Collider collider)
     {
         if (collider == null) return;
         HitSurface hitSurface = collider.GetComponent<HitSurface>();
+        if (hitSurface == null) return;  // Added check to prevent null reference
         var effectRotation = Quaternion.LookRotation(collider.transform.forward);
 
         hitMarkerPrefab = HitEffectManager.Instance.effectMap[(int)hitSurface.surfaceType].effectPrefab;
-       
+
         Instantiate(hitMarkerPrefab, collider.transform.position, effectRotation);
     }
+
+    //public void ShowHitEffect(RaycastHit hit)
+    //{
+    //    Debug.Log("===weapon show hit effect enter");
+    //    if (hit.collider == null) return;
+    //    HitSurface hitSurface = hit.collider.GetComponent<HitSurface>();
+    //    if (hitSurface == null) return;
+
+
+    //    var effectRotation = Quaternion.LookRotation(hit.normal);
+
+    //    hitMarkerPrefab = HitEffectManager.Instance.effectMap[(int)hitSurface.surfaceType].effectPrefab;
+
+    //    Instantiate(hitMarkerPrefab, hit.point, effectRotation);
+    //}
 
 }
